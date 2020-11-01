a1cf133f01c83f7bb11fbcd15b43f2e0450ccd93
NaikDasharath07 <73283803+NaikDasharath07@users.noreply.github.com>
2020-11-02 02:02:21 +0530

    api call done

--- My FlexiStay/My FlexiStay/MainView/RegisterController/ModelFile/NetworkManage.swift
+++ My FlexiStay/My FlexiStay/MainView/RegisterController/ModelFile/NetworkManage.swift
@@ -11,28 +11,177 @@ import UIKit
 import Alamofire
 
 class NetworkManagerGetOTP {
-    func GetOTPCall(urlString:URL){
+
+    // getCountry Call
+    func GetConutyId(urlString:URL){
         AF.request(urlString).responseJSON { (response) in
             
             DataModel.MainDict = response.value as! NSDictionary
-            DataModel.ArrData = DataModel.MainDict.value(forKey: "otp_type") as! NSDictionary
-            DataModel.otpLength = DataModel.ArrData.value(forKey: "length") as! Int
-            DataModel.expirationTime = DataModel.ArrData.value(forKey: "expiration") as! Int
-            DataModel.resendTime = DataModel.ArrData.value(forKey: "resend_interval") as! Int
-//            print("\(DataModel.ArrData)")
-//            print(DataModel.MainDict)
-            print("length = \(DataModel.otpLength)")
-            print("expiration = \(DataModel.expirationTime)")
-            print("resend_interval = \(DataModel.resendTime)")
+            DataModel.ArrData = DataModel.MainDict.value(forKey: "country") as! NSDictionary
+            DataModel.country_Id = DataModel.ArrData.value(forKey: "id") as! Int
+            print("Country Id\(DataModel.country_Id)")
         }
     }
+    
+   // Send OTP Call
+    func SendOTPCall(){
+        guard  let urlString:URL = URL(string: "https://acabc9536i.execute-api.ap-south-1.amazonaws.com/development/sendOtp")else{return}
+         
+        var urlRequest = URLRequest.init(url: urlString, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30)
+        urlRequest.httpMethod = "POST"
+        var sendOtpParameter = ["country_id":1,"mobile": 9606349900]
+
+       // urlRequest.httpBody = sendOtpParameter.data(using: .utf8)
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: sendOtpParameter, options: []) else{return}
+              urlRequest.httpBody = httpBody
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            
+            if  data != nil && error == nil{
+                
+                do{
+              
+                    let response = try JSONSerialization.jsonObject(with: data!, options: .mutableLeaves) as! NSDictionary
+                  print(response)
+                }catch{
+                    print(error.localizedDescription)
+                }
+            }else{
+                print("Data nil or error")
+            }
+        }.resume()
+
+        
+    }
+    // ValideOTP Call
+    func ValideOTPCall(){
+        guard  let urlString:URL = URL(string: "https://acabc9536i.execute-api.ap-south-1.amazonaws.com/development/validateOtp")else{return}
+         
+        var urlRequest = URLRequest.init(url: urlString, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30)
+        urlRequest.httpMethod = "POST"
+     var validatioOtpPara = [ "country_id": 1,"mobile": 9309924851,"otp": 174730]
+       // urlRequest.httpBody = sendOtpParameter.data(using: .utf8)
+       
+       // urlRequest.httpBody = validatioOtpPara.percentEncoded()
+
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: validatioOtpPara, options: []) else{return}
+        urlRequest.httpBody = httpBody
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            
+            if  data != nil && error == nil{
+                
+                do{
+              
+                    let responseDict = try JSONSerialization.jsonObject(with: data!, options: .mutableLeaves) as! NSDictionary
+                    print(responseDict)
+                //    let countryId = response.value(forKey: "country_id") as! Int
+                    //print(countryId)
+                }catch{
+                    print(error.localizedDescription)
+                }
+            }else{
+                print("Data nil or error")
+            }
+        }.resume()
+
+        
+    }
+    // Get OTP Call
+    func GetOTPCall(){
+           guard  let urlString:URL = URL(string: "https://acabc9536i.execute-api.ap-south-1.amazonaws.com/development/getOtpType")else{return}
+            
+           var urlRequest = URLRequest.init(url: urlString, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30)
+          
+        urlRequest.httpMethod = "POST"
+        var GetOTPParameter = ["code":"MV"]
+
+       guard let httpBody = try? JSONSerialization.data(withJSONObject: GetOTPParameter, options: []) else{return}
+        urlRequest.httpBody = httpBody
+           URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+               
+               if  data != nil && error == nil{
+                   
+                   do{
+                 
+                       let response = try JSONSerialization.jsonObject(with: data!, options: .mutableLeaves) as! NSDictionary
+                       print(response)
+                   }catch{
+                       print(error.localizedDescription)
+                   }
+               }else{
+                   print("Data nil or error")
+               }
+           }.resume()
+
+           
+       }
+    func RegisterUserl(){
+        guard  let urlString:URL = URL(string: "https://acabc9536i.execute-api.ap-south-1.amazonaws.com/development/validateOtp")else{return}
+         
+        var urlRequest = URLRequest.init(url: urlString, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30)
+        urlRequest.httpMethod = "POST"
+        let registerUser = ["user_type_id": 4,"first_name": RegisterInputValue.firstName,"last_name": RegisterInputValue.lastName,"country_id": 1,"mobile": RegisterInputValue.mobileNumber,"email": "shankar.andanappa@gmail.com","password": "100000:711c845245d5563ba577d574993bc7be:7458b5e51b09a9d76fa3776999e010fe38e633fc732bd5c0e83b6ea1fb082a99f351d44162861bcd77cca40072f40d3c5421498ade9068ea0fe88b44e60d6e96"] as [String : Any]
+       // urlRequest.httpBody = sendOtpParameter.data(using: .utf8)
+       
+       // urlRequest.httpBody = validatioOtpPara.percentEncoded()
+
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: registerUser, options: []) else{return}
+        urlRequest.httpBody = httpBody
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            
+            if  data != nil && error == nil{
+                
+                do{
+              
+                    let responseDict = try JSONSerialization.jsonObject(with: data!, options: .mutableLeaves) as! NSDictionary
+                    print(responseDict)
+                //    let countryId = response.value(forKey: "country_id") as! Int
+                    //print(countryId)
+                }catch{
+                    print(error.localizedDescription)
+                }
+            }else{
+                print("Data nil or error")
+            }
+        }.resume()
+
+        
+    }
+    
 }
+
+
+
+
 struct DataModel{
+
+    // GetOTP
     static var MainDict:NSDictionary = NSDictionary()
     static var ArrData:NSDictionary = NSDictionary()
     static var otpLength = 0
     static var expirationTime = 0
     static var resendTime = 0
-    static var urlGetOTP:URL = URL(string: "https://acabc9536i.execute-api.ap-south-1.amazonaws.com/development//getOtpType")!
+    
+    //get Country
+    static var country_Id:Int = 0
+    
+    static var getCountryUrl:URL = URL(string: "https://acabc9536i.execute-api.ap-south-1.amazonaws.com/development//getCountry")!
+    
+    
+    
+    
+    
+    // Post URL
+    static var urlGetOTP:URL = URL(string: "https://acabc9536i.execute-api.ap-south-1.amazonaws.com/development/getOtpType")!
+    static var sendOTPUrl:URL = URL(string: "https://acabc9536i.execute-api.ap-south-1.amazonaws.com/development/sendOtp")!
+    static var validateOTPUrl:URL = URL(string: "https://acabc9536i.execute-api.ap-south-1.amazonaws.com/development/validateOtp")!
+  
+    static var GetOTPParameter = ["code": "MV"]
+    static var sendOtpParameter = ["country_id":"1","mobile": "9606349900"]
+}
+struct RegisterInputValue {
+    static var firstName = ""
+    static  var lastName = ""
+    static  var mobileNumber = ""
+    static  var password = ""
     
 }
--- My FlexiStay/My FlexiStay/MainView/RegisterController/RegisterViewController.swift
+++ My FlexiStay/My FlexiStay/MainView/RegisterController/RegisterViewController.swift
@@ -22,14 +22,34 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var topImage: UIImageView!
     @IBOutlet weak var validtion: UILabel!
     
+    @IBOutlet weak var fiveView: UIView!
+    @IBOutlet weak var frouthView: UIView!
+    @IBOutlet weak var thirdView: UIView!
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var seconVIew: UIView!
+    
+    var iconClick = true
+    
     var CommontextFields = [UITextField]()
+    
+    
     var count = 30
     var timer = Timer()
-     var intanceGetOTPCall = NetworkManagerGetOTP()
+    var intanceGetOTPCall = NetworkManagerGetOTP()
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        
+        
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "passTogle"), for: .normal)
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 10, right: 0)
+        button.frame = CGRect(x: CGFloat(PasswordTextField.frame.size.width - 25), y: CGFloat(5), width: CGFloat(10), height: CGFloat(10))
+        button.addTarget(self, action: #selector(iconAction(sender:)), for: .touchUpInside)
+        PasswordTextField.rightView = button
+        PasswordTextField.rightViewMode = .always
+        
         
         DesignView.layer.borderWidth = 2
         DesignView.layer.shadowColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
@@ -41,50 +61,90 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
         validtion.isHidden = true
         Continue.layer.cornerRadius = 10
         Continue.clipsToBounds = true
+        PasswordTextField.textContentType = .oneTimeCode
+        //  let btn = UIButton()
+        // btn.setImage(UIImage(named: "icons8-checkmark-64 (1)"), for: .normal)
+        
+        // self.PasswordTextField.setupRightImage(imageName: "\(self.view.addSubview(btn))" )
+        // self.waterSuplyTxt.setupRightImage(imageName: "down-arrow (1)")
         CommontextFields = [firstNameTextField, LastName,mobileTextField, otpEnterTextfield, PasswordTextField]
-                        CommontextFields.forEach {
-                            $0.delegate = self
-
-                
-                        }
-        intanceGetOTPCall.GetOTPCall(urlString: DataModel.urlGetOTP)
+        CommontextFields.forEach {
+            $0.delegate = self
+            
+            
+        }
+        
+        intanceGetOTPCall.GetConutyId(urlString: DataModel.getCountryUrl)
+        //intanceGetOTPCall.SendOTPCallPost(urlString: DataModel.sendOTPUrl)
+        //  intanceGetOTPCall.VaidationOTPCallPost(urlString: DataModel.validateOTPUrl)
+        //  intanceGetOTPCall.GetOTPCallPost(urlString: DataModel.urlGetOTP)
+        //  intanceGetOTPCall.withoutAlmoFire()
+        //  intanceGetOTPCall.ValideOTPCall()
+        //     intanceGetOTPCall.GetOTPCall()
+        // intanceGetOTPCall.SendOTPCall()
         
     }
+    
+    @IBAction func contiueTORegiserUser(_ sender: UIButton) {
+        
+        if firstNameTextField.text != nil{
+            RegisterInputValue.firstName = firstNameTextField.text!
+        }else if LastName.text != nil{
+            RegisterInputValue.lastName = LastName.text!
+        }else if mobileTextField.text != nil{
+            RegisterInputValue.mobileNumber = mobileTextField.text!
+        }
+        if firstNameTextField.text != nil && LastName.text != nil &&  (mobileTextField.text != nil){
+            intanceGetOTPCall.RegisterUserl()
+            firstView.layer.backgroundColor =  #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            
+            seconVIew.layer.backgroundColor =  #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            thirdView.layer.backgroundColor =  #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            frouthView.layer.backgroundColor =  #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            fiveView.layer.backgroundColor =  #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        }
+        
+    }
+    @IBAction func iconAction(sender: UIButton) {
+        if( iconClick == true) {
+            PasswordTextField.isSecureTextEntry = true
+        } else {
+            PasswordTextField.isSecureTextEntry = false
+        }
+        
+        iconClick = !iconClick
+    }
+    
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
         
-                   switch textField{
-                   case mobileTextField:
-                        otpTimer.isHidden = false
-                        sendOtp.isHidden = false
-                       mobileTextField.keyboardType = .numberPad
-                       mobileTextField.enablesReturnKeyAutomatically = true
-                   
-                   default:
-                       break
-                   }
+        //        if mobileTextField.text?.isEmpty !== true {
+        //
+        //        }
     }
     
     @IBAction func sendOTP(_ sender: UIButton) {
-
+        
         otpTimer.isHidden = false
+        
         print("click")
-       timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
-        }
-
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+    }
+    
     @objc func countDown() {
         otpTimer.text = "\(count)"
         if count == 0 {
             sendOtp.setTitle("Resent OTP", for: .normal)
             timer.invalidate()
             count = 30
-
+            
         }
+        
         count -= 1
-
     }
-  
+    
 }
+
 
 extension RegisterViewController:UITextViewDelegate{
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -96,6 +156,8 @@ extension RegisterViewController:UITextViewDelegate{
         }else if textField == LastName{
             mobileTextField.becomeFirstResponder()
         }else if textField == mobileTextField{
+            sendOtp.isHidden = true
+            
             otpEnterTextfield.becomeFirstResponder()
         }else if textField == otpEnterTextfield{
             PasswordTextField.becomeFirstResponder()
@@ -106,11 +168,11 @@ extension RegisterViewController:UITextViewDelegate{
     }
     func textFieldDidEndEditing(_ textField: UITextField) {
         CommontextFields = [firstNameTextField, LastName,mobileTextField, otpEnterTextfield, PasswordTextField]
-                CommontextFields.forEach {
-                    $0.text = " "
-
+        CommontextFields.forEach {
+            $0.text = " "
+            
+            
+        }
         
-                }
-       
     }
 }
